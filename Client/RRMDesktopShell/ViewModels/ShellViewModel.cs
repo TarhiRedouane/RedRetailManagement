e9642cb53435fb62fe8754014ad805d2e4bb3ab9
Tarhi Mohamed Redouane <red.skull911@hotmail.com>
2020-03-27 19:38:32 +0100

    get the instance of loginViewModel from IOC Instance

--- Client/RRMDesktopShell/ViewModels/ShellViewModel.cs
+++ Client/RRMDesktopShell/ViewModels/ShellViewModel.cs
@@ -5,17 +5,16 @@ namespace RRMDesktopShell.ViewModels
 {
     public class ShellViewModel : Conductor<object>,IHandle<LoggedInEvent>
     {
-        private LoginViewModel _loginViewModel;
+
         private readonly SalesViewModel _salesViewModel;
         private readonly IEventAggregator _eventAggregator;
 
-        public ShellViewModel(LoginViewModel loginViewModel,SalesViewModel salesViewModel,IEventAggregator eventAggregator)
+        public ShellViewModel(SalesViewModel salesViewModel,IEventAggregator eventAggregator)
         {
-            _loginViewModel = loginViewModel;
             _salesViewModel = salesViewModel;
             _eventAggregator = eventAggregator;
             _eventAggregator.Subscribe(this);
-            ActivateItem(loginViewModel);
+            ActivateItem(IoC.Get<LoginViewModel>());
         }
 
         public void Handle(LoggedInEvent message)
